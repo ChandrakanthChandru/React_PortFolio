1feb39af8c25f441ac5db33582eb05d0ba202ea6
ChandrakanthChandru <chandruchandrakanth2004@gmail.com>
2026-02-07 12:49:58 +0530

    feat: initialize main application component with React Router and define core routes for navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,20 @@ import Skills from "./Skills";
 import Certification from "./Certification";
 import Contact from "./Contact";
 import Project from "./Project";
-import './app.css'
-
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />             {/* Default route */}
+        <Route path="/" element={<Home />} /> {/* Default route */}
         <Route path="/about" element={<Home />} />
         <Route path="/education" element={<Education />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/certification" element={<Certification />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/project" element={<Project/>} />
-        
+        <Route path="/project" element={<Project />} />
       </Routes>
     </BrowserRouter>
   );
